3a2ffb50366b0e068142e67c6717eef2745937f4
Rajath Srinivasa <rajath.srinivasa@n2nservices.com>
2023-10-03 10:37:38 -0400

    shell script assessment

---
+++ assess1.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# the below three code lines block updates the package manager, installs mongoDB and restarts it.
+sudo apt update
+# the -y option implies 'Yes' to the installation process
+sudo apt install -y mongodb
+sudo systemctl restart mongodb
+
+# mongoDB follows a lazy creation approach
+mongo <<EOF
+use mydatabase
+db.createUser({
+  user: "admin",
+  pwd: "admin",
+  roles: [{ role: "read", db: "mydatabase" }] 
+})
+exit
+EOF
+
+# Step 4: Provide Feedback
+echo "MongoDB installation and database setup completed."
+echo "Database: mydatabase"
+echo "User: myuser"
+echo "Password: mypassword"
